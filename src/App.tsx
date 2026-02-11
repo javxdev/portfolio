654ddf698bebcb1aed7b899a473a1ae189bd014c
Jan Alexis <javxdev97@gmail.com>
2026-02-11 14:51:44 +0400

    Added web analytics from Vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { Analytics } from '@vercel/analytics/react';
 
 // Components
 import { Cursor } from './components/Cursor/Cursor';
@@ -33,6 +34,7 @@ function App() {
 
   return (
     <>
+    <Analytics />
       <Cursor />
       <Navigation 
         scrollTo={scrollTo} 
